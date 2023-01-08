093804b8913b3378923e7b1c9814940df521ff9d
Andrew Keenan Richardson <richardson.andrew@gmail.com>
2023-01-08 15:22:43 -0800

    Make them different colors

--- Evosim/Assets/FishMover.cs
+++ Evosim/Assets/FishMover.cs
@@ -9,6 +9,8 @@ public class FishMover : MonoBehaviour
     public float mass = 1;
     public GameObject myCapsule;
     public Color myColor = Color.white;
+    
+    float eatTimer = 1; // Seconds
 
     // Start is called before the first frame update
     void Start()
@@ -16,21 +18,36 @@ public class FishMover : MonoBehaviour
     }
     
     
-    void OnCollisionEnter(Collision col) {
+    void OnCollisionEnter(Collision col)
+    {
+        if (eatTimer > 0)
+            return; // Eating is so tiring sometimes
+        
         Debug.Log("I collided!");
 
         var otherFish = col.gameObject;
 
+        if (myColor == otherFish.GetComponent<FishMover>().myColor)
+            return; // Don't eat family
+
         float myScore = mass;
         float theirScore = otherFish.GetComponent<FishMover>().mass;
 
-        if (myScore < theirScore)
-            gameObject.SetActive(false);
-        else
+        if (myScore > theirScore)
         {
-            mass += theirScore;
+            otherFish.SetActive(false);
             direction += direction.normalized * 0.2f;
-            gameObject.transform.localScale = new Vector3(1f, 1f, 1f) * Mathf.Pow(mass, 0.333f);
+            mass += theirScore;
+
+            if (mass >= 5)
+            {
+                mass -= 4f;
+                eatTimer = 2f;
+                for (int i = 0; i < 4; i++)
+                {
+                    Startup.CreateFish(gameObject.transform.position, myColor);
+                }
+            }
         }
     }
 
@@ -44,16 +61,22 @@ public class FishMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        eatTimer -= Time.deltaTime;
+        
         if (Time.frameCount % 100 == moveTick)
             direction = direction + (new Vector3(Random.value, Random.value, Random.value) 
                                      - new Vector3(0.5f, 0.5f, 0.5f)) * Time.deltaTime * 100f;
         gameObject.transform.position = gameObject.transform.position + direction * Time.deltaTime;
         gameObject.transform.forward = Vector3.Normalize(direction);
 
+        // Keep them in a sphere
         if (gameObject.transform.position.magnitude > 25f)
         {
             gameObject.transform.position = gameObject.transform.position.normalized * 25f;
             direction = direction * -1;
         }
+        
+        // Set size
+        gameObject.transform.localScale = new Vector3(1f, 1f, 1f) * Mathf.Pow(mass, 0.333f);
     }
 }
--- Evosim/Assets/Startup.cs
+++ Evosim/Assets/Startup.cs
@@ -6,19 +6,28 @@ public class Startup : MonoBehaviour
 {
     
     public GameObject fishPrototype;
+    private static Startup mainInstance;
+
+    public static void CreateFish(Vector3 pos, Color color)
+    {
+        var fish = GameObject.Instantiate(mainInstance.fishPrototype);
+        fish.transform.position = pos;
+        fish.GetComponent<FishMover>().moveTick = Random.Range(0, 100);
+        // var mat = new Material();
+        fish.GetComponent<FishMover>().SetColor(color);
+        fish.GetComponent<FishMover>().mass = Random.Range(0.9f, 1.1f);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
+        mainInstance = this;
         Debug.Log("Hello fish!");
         for (int i = 0; i < 100; i++)
         {
-            var fish = GameObject.Instantiate(fishPrototype);
-            fish.transform.position = (new Vector3(Random.value, Random.value, Random.value) 
-                                       - new Vector3(0.5f, 0.5f, 0.5f)) * 20 ;
-            fish.GetComponent<FishMover>().moveTick = Random.Range(0, 100);
-            // var mat = new Material();
-            fish.GetComponent<FishMover>().SetColor(new Color(Random.value, Random.value, Random.value));
+            CreateFish((new Vector3(Random.value, Random.value, Random.value) 
+                        - new Vector3(0.5f, 0.5f, 0.5f)) * 20, 
+                new Color(Random.value, Random.value, Random.value));
         }
     }
 
